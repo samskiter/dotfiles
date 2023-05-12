cdb8a0ce1cba1acc23f386e2f8898e9848e99f8f
Sam Duke <samskiter@users.noreply.github.com>
2023-05-12 11:44:18 +0100

    Make dotfiles auto install if on codespaces

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ function doIt() {
 	source ~/.bash_profile;
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" == "--force" -o "$1" == "-f" -o "$CODESPACES" == "true" ]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
